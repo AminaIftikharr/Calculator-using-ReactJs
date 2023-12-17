d95f483660bbef68df3e10a0efef8fe2faea63d9
Amina Iftikhar <144228946+AminaIftikharr@users.noreply.github.com>
2023-12-17 16:04:47 +0500

    added css to button

--- calculator/src/App.js
+++ calculator/src/App.js
@@ -3,6 +3,7 @@ import Button from './components/button';
 import Input from './components/input';
 
 const App= ()=> {
+  const buttoncolor ="#f2a33c";
   return (
     <div className="App">
       <div className="calc-wrapper">
@@ -11,26 +12,27 @@ const App= ()=> {
           <Button symbol="7"/>
           <Button symbol="8"/>
           <Button symbol="9"/>
-          <Button symbol="/"/>
+          <Button symbol="/" color={buttoncolor}/>
         </div>
         <div className="rows">
           <Button symbol="4"/>
           <Button symbol="5"/>
           <Button symbol="6"/>
-          <Button symbol="*"/>
+          <Button symbol="*" color={buttoncolor}/>
         </div>
         <div className="rows">
           <Button symbol="1"/>
           <Button symbol="2"/>
           <Button symbol="3"/>
-          <Button symbol="+"/>
+          <Button symbol="+" color={buttoncolor}/>
         </div>
         <div className="rows">
           <Button symbol="0"/>
           <Button symbol="."/>
           <Button symbol="="/>
-          <Button symbol="-"/>
+          <Button symbol="-" color={buttoncolor}/>
         </div>
+        <Button symbol="Clear" />
       </div>
     </div>
   );
--- calculator/src/components/button.js
+++ calculator/src/components/button.js
@@ -1,7 +1,7 @@
 import "./button.css";
 
-const button = ({symbol}) => {
-    return <div className="button-wrapper">{symbol}</div>
+const button = ({symbol, color}) => {
+    return <div className="button-wrapper" style={{backgroundColor: color}}>{symbol}</div>
 };
 
 export default button;
