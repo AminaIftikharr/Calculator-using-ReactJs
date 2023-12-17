a8eeca5803790a8513b683406f88f02072d8fa67
Amina Iftikhar <144228946+AminaIftikharr@users.noreply.github.com>
2023-12-17 15:49:09 +0500

    uploading basic structure

---
+++ calculator/src/App.js
@@ -0,0 +1,39 @@
+import './App.css';
+import Button from './components/button';
+import Input from './components/input';
+
+const App= ()=> {
+  return (
+    <div className="App">
+      <div className="calc-wrapper">
+        <Input />
+        <div className="rows">
+          <Button symbol="7"/>
+          <Button symbol="8"/>
+          <Button symbol="9"/>
+          <Button symbol="/"/>
+        </div>
+        <div className="rows">
+          <Button symbol="4"/>
+          <Button symbol="5"/>
+          <Button symbol="6"/>
+          <Button symbol="*"/>
+        </div>
+        <div className="rows">
+          <Button symbol="1"/>
+          <Button symbol="2"/>
+          <Button symbol="3"/>
+          <Button symbol="+"/>
+        </div>
+        <div className="rows">
+          <Button symbol="0"/>
+          <Button symbol="."/>
+          <Button symbol="="/>
+          <Button symbol="-"/>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ calculator/src/components/button.js
@@ -0,0 +1,7 @@
+import "./button.css";
+
+const button = ({symbol}) => {
+    return <div className="button-wrapper">{symbol}</div>
+};
+
+export default button;
